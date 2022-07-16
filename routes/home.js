99380e91b5956ecf5a73aff3e7516e4e63a9ddc2
K Gordon <krisan_gordon@yahoo.com>
2022-07-16 08:33:40 -0700

    updated style sheet

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,7 @@ var conn = require('../lib/dbConnections');
 //renders longin view
 router.get('/', function(req, res, next) {
     // res.render('../views/home', {my_session: req.session});
-    res.render('../views/publicViews/home');
+    res.render('../views/publicViews/home', {my_session: req.session});
 
 });
 
